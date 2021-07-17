744a1061aea39da5c19c3bbc02b69359fa97cd96
wulongkuan <979610327@qq.com>
2021-07-18 00:30:51 +0800

    feat: test prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 const App: React.FC = () => {
-  useEffect(() => {
-    
-  })
+  useEffect(() => {});
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +22,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
